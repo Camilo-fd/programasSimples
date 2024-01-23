0bef43bb6b041b917e1e0114c91e4a1a2ce16e43
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-01-23 13:31:18 -0500

    correcion del codigo

--- 9.queNotaNecesito.py
+++ 9.queNotaNecesito.py
@@ -1,9 +1,9 @@
+import math
 print("Escribe acontinuacion los certamnes y el laboratorio")
 c1 = int(input("Nota certamen 1: "))
 c2 = int(input("Nota certamen 2: "))
-c3 = 0
 lab = int(input("Nota Laboratorio: "))
 Nl = lab
-Nc = int((c1 + c2) / 2)
-Nf =  float(round((60 - (Nc * 0.7 + Nl * 0.3)) / 0.7,2))
-print(f"Necesita nota {Nf} en el certamen 3")
+Nc = (59.6 - Nl * 0.3) / 0.7
+c3 = 3 * Nc - c1 - c2 
+print(f"Necesita nota {math.ceil(c3)} en el certamen 3")
